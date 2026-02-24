cae7ba1b8697563c60873becf95491e7cdad264e
Lance Thongsavanh <lancer8106@gmail.com>
2026-02-23 19:38:53 -0500

    Nested the while loop for valid input inside another while loop that checks if the user entered the 'quit' value.

--- unit-7-participation.py
+++ unit-7-participation.py
@@ -5,13 +5,20 @@ Set up the while loop with a flag
 Use a guard clause to validate input to take only strings - provide a message and continue if it isn't a string
 """
 
+user_input: str = ""
 correct_input: bool = False
-while not correct_input:
-    topping: str = input("Enter a pizza topping (enter 'quit' to end): ")
 
-    if topping.isdigit():
-        print("Error: Invalid Input.")
-        continue
+while user_input != "n":
 
-    print(f"{topping} will be added to your pizza.")
-    correct_input = True
+    while not correct_input:
+        user_input = input("Enter a pizza topping: ")
+
+        if user_input.isdigit():
+            print("Error: Invalid Input.")
+            continue
+
+        print(f"{user_input} will be added to your pizza.")
+        correct_input = True
+    
+    user_input = input("Would you like to add more toppings? y/n: ")
+    correct_input = False
