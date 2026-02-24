8b886b1d410dd4476225947e947ccfcc3955c1bc
Lance Thongsavanh <147730520+LanceTho@users.noreply.github.com>
2026-02-23 19:16:01 -0500

    Create unit-7-participation.py

---
+++ unit-7-participation.py
@@ -0,0 +1,6 @@
+"""
+7-4. Pizza Toppings: Write a loop that prompts the user to enter a series of pizza toppings until they enter a 'quit' value. As they enter each topping, print a message saying you’ll add that topping to their pizza.
+Reqirements:
+Set up the while loop with a flag
+Use a guard clause to validate input to take only strings - provide a message and continue if it isn't a string
+"""
